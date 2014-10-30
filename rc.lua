9580a0438200a73b3a16e0be65983b8889fd21ba
Mich <michel.nemnom@gmail.com>
2014-10-30 16:04:34 +0100

    Obvious

--- rc.lua
+++ rc.lua
@@ -14,27 +14,7 @@ local menubar = require("menubar")
 -- Load Debian menu entries
 require("debian.menu")
 vicious = require("vicious")
-
--- Initialize widgets
-sepwidget = wibox.widget.textbox()
-sepwidget:set_text(" ")
-
-cpuwidget = awful.widget.graph()
-cpuwidget:set_width(50)
-cpuwidget:set_background_color("#494B4F")
-cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
-                    {1, "#AECF96" }}})
-vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
-
-memwidget = awful.widget.progressbar()
-memwidget:set_width(8)
-memwidget:set_height(10)
-memwidget:set_vertical(true)
-memwidget:set_background_color("#494B4F")
-memwidget:set_border_color(nil)
-memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#AECF96"}, {0.5, "#88A175"}, 
-                    {1, "#FF5656"}}})
-vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
+obvious = require("obvious")
 
 -- {{{ Error handling
 -- Check if awesome encountered an error during startup and fell back to
@@ -134,6 +114,34 @@ mylauncher = awful.widget.launcher({ image = beautiful.awesome_icon,
 menubar.utils.terminal = terminal -- Set the terminal for applications that require it
 -- }}}
 
+-- {{{ Initialize widgets
+-- Separator
+local sepwidget = wibox.widget.textbox()
+sepwidget:set_text(" ")
+
+-- CPU
+local cpuwidget = awful.widget.graph()
+cpuwidget:set_width(50)
+cpuwidget:set_background_color("#494B4F")
+cpuwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#FF5656"}, {0.5, "#88A175"}, 
+                    {1, "#AECF96" }}})
+vicious.register(cpuwidget, vicious.widgets.cpu, "$1")
+
+-- RAM
+local memwidget = awful.widget.progressbar()
+memwidget:set_width(8)
+memwidget:set_height(10)
+memwidget:set_vertical(true)
+memwidget:set_background_color("#494B4F")
+memwidget:set_border_color(nil)
+memwidget:set_color({ type = "linear", from = { 0, 0 }, to = { 10,0 }, stops = { {0, "#AECF96"}, {0.5, "#88A175"}, 
+                    {1, "#FF5656"}}})
+vicious.register(memwidget, vicious.widgets.mem, "$1", 13)
+
+require("obvious.volume_alsa")
+
+-- }}}
+
 -- {{{ Wibox
 -- Create a textclock widget
 mytextclock = awful.widget.textclock()
@@ -221,6 +229,8 @@ for s = 1, screen.count() do
     right_layout:add(cpuwidget)
     right_layout:add(sepwidget)
     right_layout:add(memwidget)
+    right_layout:add(sepwidget)
+    right_layout:add(obvious.volume_alsa(0, "Master"))
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
